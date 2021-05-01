32e36c4dc4985e632bbac96a269db9df4d120f69
Clailson Lucas <clailsonlucas@gmail.com>
2021-05-01 20:41:14 -0300

    Retweets and likes

--- lib/chirp_web/live/post_live/post_component.ex
+++ lib/chirp_web/live/post_live/post_component.ex
@@ -19,10 +19,14 @@ defmodule ChirpWeb.PostLive.PostComponent do
 
       <div class="row">
         <div class=column>
-          <i class="far fa-heart"></i> <%= @post.likes_count %>
+          <a href="#" phx-click="like" phx-target="<%= @myself %>">
+            <i class="far fa-heart"></i> <%= @post.likes_count %>
+          </a>
         </div>
         <div class=column>
-          <i class="fa fa-retweet"></i> <%= @post.reposts_count %>
+          <a href="#" phx-click="repost" phx-target="<%= @myself %>">
+            <i class="fa fa-retweet"></i> <%= @post.reposts_count %>
+          </a>
         </div>
         <div class="column">
           <%= live_patch to: Routes.post_index_path(@socket, :edit, @post.id) do %>
@@ -37,4 +41,12 @@ defmodule ChirpWeb.PostLive.PostComponent do
     """
   end
 
+  def handle_event("like", _, socket) do
+    Chirp.Timeline.inc_likes(socket.assigns.post)
+    {:noreply, socket}
+  end
+  def handle_event("repost", _, socket) do
+    Chirp.Timeline.inc_reposts(socket.assigns.post)
+    {:noreply, socket}
+  end
 end
